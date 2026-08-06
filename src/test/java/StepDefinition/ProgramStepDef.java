b1c372ddf7760bae74a9a0e747bc54d06aa9f13d
PrasannaRahavi14 <Prasannarahavi14@gmail.com>
2026-08-06 16:16:40 -0400

    Program Module Feature and Pages

---
+++ src/test/java/StepDefinition/ProgramStepDef.java
@@ -0,0 +1,122 @@
+package StepDefinition;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+
+import DriverFactory.DriverFactory;
+import Pages.ProgramPage;
+import Utilities.BaseLogger;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class ProgramStepDef extends BaseLogger {
+	
+	WebDriver driver = DriverFactory.getDriver();
+    ProgramPage programpage = new ProgramPage(driver);
+    
+	@Given("Admin is on home page after Login")
+	public void admin_is_on_home_page_after_login() {
+	    programpage.DoLogin();
+	    boolean HomePageTitle = programpage.OnHomepage();
+	    log.info("HomePage is displayed : "+HomePageTitle);
+	  
+	}
+
+	@When("Admin clicks Program on the navigation bar")
+	public void admin_clicks_program_on_the_navigation_bar() {
+	    programpage.ClickProgramMenu();
+	    
+	}
+
+	@Then("Admin should be navigated to Program page")
+	public void admin_should_be_navigated_to_program_page() {
+	    boolean isDisplayed = programpage.GetManageProgram();
+	    Assert.assertTrue(isDisplayed);
+		log.info("Manage Program page is displayed : "+isDisplayed);
+	    
+	}
+
+	@Then("Admin should see sub menu in menu bar as {string}")
+	public void admin_should_see_sub_menu_in_menu_bar_as(String ExpectedSubMenu) {
+		String ActualSubMenu = programpage.AddNewProgramMenu();
+		Assert.assertEquals(ExpectedSubMenu, ActualSubMenu);
+		log.info("Expected SubMenu is : "+ActualSubMenu);
+	    
+	}
+
+	@Then("Admin should see the heading {string}")
+	public void admin_should_see_the_heading(String ExpectedHeading) {
+	    String ActualHeading = programpage.GetProgramTitle();
+	    Assert.assertEquals(ExpectedHeading, ActualHeading);
+		log.info("Expected Heading is : "+ActualHeading);
+	}
+
+	@Then("Admin should see manage program heading aligned on the left side")
+	public void admin_should_see_manage_program_heading_aligned_on_the_left_side() {
+	    //boolean isLeftAligned = programpage.isManageProgramLeftAligned();
+	    //Assert.assertTrue(isLeftAligned);
+		Assert.assertTrue(programpage.isManageProgramLeftAligned(),"Manage Program Heading is not left aligned"); 
+	}
+
+	@Then("Admin should see a Delete button in left top is disabled")
+	public void admin_should_see_a_delete_button_in_left_top_is_disabled() {
+	    
+	    
+	}
+
+	@Then("Admin should see Search bar")
+	public void admin_should_see_search_bar() {
+	    
+	    
+	}
+
+	@Then("Admin should see search... placeholder text")
+	public void admin_should_see_search_placeholder_text() {
+	    
+	    
+	}
+
+	@Then("Admin should see data table with column header")
+	public void admin_should_see_data_table_with_column_header() {
+	    
+	    
+	}
+
+	@Then("Admin should see checkbox default state as unchecked beside Program Name column header")
+	public void admin_should_see_checkbox_default_state_as_unchecked_beside_program_name_column_header() {
+	    
+	    
+	}
+
+	@Then("Admin should see check box default state as unchecked on the left side in all rows against program name")
+	public void admin_should_see_check_box_default_state_as_unchecked_on_the_left_side_in_all_rows_against_program_name() {
+	    
+	    
+	}
+
+	@Then("Admin should see the sort arrow icon beside to each column header except Edit and Delete")
+	public void admin_should_see_the_sort_arrow_icon_beside_to_each_column_header_except_edit_and_delete() {
+	    
+	    
+	}
+
+	@Then("Admin should see the Edit and Delete buttons on each row of the data table")
+	public void admin_should_see_the_edit_and_delete_buttons_on_each_row_of_the_data_table() {
+	    
+	    
+	}
+	
+	@Then("Admin should see the text as {string} along with Pagination controls below the table.")
+	public void admin_should_see_the_text_as_along_with_pagination_controls_below_the_table(String string) {
+	    
+	    
+	}
+	
+	@Then("Admin should see the footer as {string}")
+	public void admin_should_see_the_footer_as(String string) {
+	    
+	    
+	}
+
+}
--- src/test/resources/Features/Program.feature
+++ src/test/resources/Features/Program.feature
@@ -1,14 +1,62 @@
-Feature: Program Page validation 
+Feature: Program Navigation validation 
 
 Background:
 Given Admin is on home page after Login
+When Admin clicks Program on the navigation bar
 
 @ProgramPageNavigation
 Scenario:
-When Admin clicks "Program" on the navigation bar
 Then Admin should be navigated to Program page
 
 @SubMenuDisplay
 Scenario:
-When Admin clicks "Program" on the navigation bar
 Then Admin should see sub menu in menu bar as "Add New Program"
+
+@ManageProgramHeading
+Scenario:
+Then Admin should see the heading "Manage Program"
+
+@ManageProgramAlignment
+Scenario:
+Then Admin should see manage program heading aligned on the left side
+
+@DeleteButtonState
+Scenario:
+Then Admin should see a Delete button in left top is disabled
+
+@SearchBoxDisplay
+Scenario:
+Then Admin should see Search bar
+
+@SearchPlaceholder
+Scenario:
+Then Admin should see search... placeholder text
+
+@ColumnHeaderName
+Scenario:
+Then Admin should see data table with column header
+
+@CheckboxHeaderState
+Scenario:
+Then Admin should see checkbox default state as unchecked beside Program Name column header
+
+@CheckboxEachRow
+Scenario:
+Then Admin should see check box default state as unchecked on the left side in all rows against program name 
+
+@SortIconPrescence
+Scenario:
+Then Admin should see the sort arrow icon beside to each column header except Edit and Delete
+
+@Edit/DeleteIcon
+Scenario:
+Then Admin should see the Edit and Delete buttons on each row of the data table
+
+@PaginationControl
+Scenario:
+Then Admin should see the text as "Showing x to y of z entries" along with Pagination controls below the table.
+
+@FooterMessage
+Scenario:
+Then Admin should see the footer as "In total there are z programs"
+ 
